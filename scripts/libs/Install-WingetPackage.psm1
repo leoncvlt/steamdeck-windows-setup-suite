eb5e81d6fb2532c30f8fbdb07e257914f7fc5435
Leonardo Cavaletti <impeto.blu@gmail.com>
2023-10-03 18:27:17 +0100

    Removed winget package existence check

--- scripts/libs/Install-WingetPackage.psm1
+++ scripts/libs/Install-WingetPackage.psm1
@@ -9,15 +9,6 @@ function Install-WingetPackage {
         Install-DownloadPackage -Path "https://api.github.com/repos/microsoft/winget-cli/releases/latest" -Match ".msixbundle"
     }
 
-    $AlreadyInstalled = $false
-    foreach ($line in (Invoke-Expression "$winget list")) {
-        if ($line -like "*$PackageId*") {
-            $AlreadyInstalled = $true
-            break
-        }
-    }
-    if (-not $AlreadyInstalled) {
-        Write-Host "Installing $PackageID"
-        Invoke-Expression "$winget install --accept-package-agreements --accept-source-agreements -h -e --id $PackageId --force"
-    }
+    Write-Host "Installing $PackageID"
+    Invoke-Expression "$winget install --accept-package-agreements --accept-source-agreements -h -e --id $PackageId --force"
 }
